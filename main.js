eef6290cf15ee22bdaf888a4ed4b7687008ec387
Thiago Bittencourt <thiagobitt86@gmail.com>
2022-08-31 15:54:44 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,59 @@
+'use strict';
+
+async function init() {
+    const adId = document.querySelector('meta[name="twitter:app:url:iphone"]')?.content.split('id=')[1];
+
+    const apiResponse = await sendGetRequest(`https://api.mercadolibre.com/items?ids=${adId}`);
+
+    const {
+        body: {start_time},
+    } = apiResponse[0] || null;
+
+    const today = new Date();
+    const oneDay = 24 * 60 * 60 * 1000; // h * m * s * m
+
+    const adStartTime = new Date(start_time);
+    const adDiffDays = Math.round(Math.abs(adStartTime - today) / oneDay);
+
+    const container = document.querySelector('.ui-pdp-header__subtitle');
+
+    // o site do ML executa um refresh na página depois de 1,5 segundos. aguarde esse tempo para popular o DOM.
+    setTimeout(() => {
+        container.insertAdjacentHTML(
+            'beforebegin',
+            `
+            <ul class="mltext-container">
+                <li>Anúncio criado em: <span>${formatDate(adStartTime)} (${adDiffDays} dias atrás)</span></li>
+            </ul>            
+            `
+        )
+    }, 1500);
+}
+
+async function sendGetRequest(url) {
+    try {
+        const config = {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+        };
+
+        const response = await fetch(url, config);
+
+        return await response.json();
+    } catch (error) {
+        console.log('Request error:', error);
+    }
+}
+
+function formatDate(date) {
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear();
+
+    return `${day}/${month}/${year}`;
+}
+
+init();
